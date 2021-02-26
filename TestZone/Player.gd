1fcbbd3a472b5d754db615760be4d1be0c0e820e
Sanasale <quentin.mallen@gmail.com>
2021-02-26 20:40:03 +0100

    Déplacement moins degueu

--- TestZone/Player.gd
+++ TestZone/Player.gd
@@ -3,20 +3,29 @@ extends KinematicBody2D
 export var controlMode = 1 # Can be 1 or 2 if player 1 or 2
 
 var VECTOR_UP = Vector2(0, -1)
-var SPEED = 100
+var MAXSPEED = 300
+var FRICTION = 0.2
+var ACCELERATION = 40
+
+var velocity : Vector2
+
+func _ready():
+	velocity = Vector2(0, 0)
+
 
 func _physics_process(delta):
-	var velocity = Vector2(0, 0)
-	if(Input.is_action_pressed(getMyInput("Right"))):
-		velocity.x += SPEED
-	if(Input.is_action_pressed(getMyInput("Left"))):
-		velocity.x -= SPEED
-	if(Input.is_action_pressed(getMyInput("Down"))):
-		velocity.y += SPEED
-	if(Input.is_action_pressed(getMyInput("Up"))):
-		velocity.y -= SPEED
+	var right = Input.is_action_pressed(getMyInput("Right"))
+	var left = Input.is_action_pressed(getMyInput("Left"))
+	var up = Input.is_action_pressed(getMyInput("Up"))
+	var down = Input.is_action_pressed(getMyInput("Down"))
+	if(right != left):
+		if(right):
+			velocity.x = clamp(velocity.x + ACCELERATION, -MAXSPEED, MAXSPEED)
+		else:
+			velocity.x = clamp(velocity.x - ACCELERATION, -MAXSPEED, MAXSPEED)
+	else:
+		velocity.x = lerp(velocity.x, 0, FRICTION)
 	velocity = move_and_slide(velocity, VECTOR_UP)
-	print(velocity)
 
 
 
