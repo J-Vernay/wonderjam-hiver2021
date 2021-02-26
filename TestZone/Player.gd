1891adb4f29bccca33094c3baee6360c35114d23
Sanasale <quentin.mallen@gmail.com>
2021-02-26 22:15:31 +0100

    L'impulse !

--- TestZone/Player.gd
+++ TestZone/Player.gd
@@ -11,6 +11,11 @@ const GRAVITY = 600
 const JUMPFORCE = 300
 
 var velocity : Vector2
+var disableImpulse = false
+
+enum direction{
+	leftUp = 0, up = 1, upRight = 2, right = 3, downLeft = 4, down = 5, downRight = 6, left = 7, none = 8
+}
 
 func _ready():
 	velocity = Vector2(0, 0)
@@ -20,8 +25,9 @@ func _physics_process(delta):
 	var right = Input.is_action_pressed(getMyInput("Right"))
 	var left = Input.is_action_pressed(getMyInput("Left"))
 	var up = Input.is_action_pressed(getMyInput("Up"))
-	var jump = Input.is_action_just_pressed(getMyInput("Up"))
+	var jump = Input.is_action_just_pressed(getMyInput("Jump"))
 	var down = Input.is_action_pressed(getMyInput("Down"))
+	var attack = Input.is_action_just_pressed(getMyInput("Attack"))
 	if(right != left):
 		if(right):
 			velocity.x = clamp(velocity.x + ACCELERATION, -MAXSPEED, MAXSPEED)
@@ -32,11 +38,58 @@ func _physics_process(delta):
 	if(jump && is_on_floor()):
 		velocity.y = -JUMPFORCE
 	velocity.y += GRAVITY * delta
+	if(velocity.x > 0):
+		$AnimatedSprite.flip_h = false
+	else:
+		$AnimatedSprite.flip_h = true
 	velocity = move_and_slide(velocity, VECTOR_UP)
+	
+	if(disableImpulse):
+		$ImpulseZone/CollisionShape2D.disabled = true
+		
+	if(attack):
+		slash(getDirection(up, right, down, left))
 
 
+func getDirection(up : bool, right : bool, down : bool, left : bool):
+	var xdir = int(right) - int(left)
+	var ydir = int(up) - int(down)
+	var dir = 3*ydir + xdir
+	if(dir == 0):
+		dir = 1 - 2 * int($AnimatedSprite.flip_h)
+	return dir
 
 
+var slashDirection
+func slash(direction):
+	disableImpulse = true
+	slashDirection = direction
+	$ImpulseZone/CollisionShape2D.disabled = false
+	#Faut placer la position de la zone maintenant
 
 func getMyInput(name : String):
 	return name + str(controlMode)
+
+
+func _on_ImpulseZone_body_entered(body):
+	var impulse = Vector2()
+	var FORCE = 500
+	match(slashDirection):
+		1:
+			impulse = Vector2.RIGHT
+		2:
+			impulse = Vector2(-1, -1)
+		3:
+			impulse = Vector2.UP
+		4:
+			impulse = Vector2(1, -1)
+		-1:
+			impulse = Vector2.LEFT
+		-2:
+			impulse = Vector2(1, 1)
+		-3:
+			impulse = Vector2.DOWN
+		-4:
+			impulse = Vector2(-1, 1)
+	
+	body.apply_central_impulse(impulse * FORCE)
