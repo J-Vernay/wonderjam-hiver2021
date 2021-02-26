107f054721b92037f3a76849242fcb86d3bf9a18
Sanasale <quentin.mallen@gmail.com>
2021-02-26 21:22:27 +0100

    Jump

--- TestZone/Player.gd
+++ TestZone/Player.gd
@@ -2,10 +2,13 @@ extends KinematicBody2D
 
 export var controlMode = 1 # Can be 1 or 2 if player 1 or 2
 
-var VECTOR_UP = Vector2(0, -1)
-var MAXSPEED = 300
-var FRICTION = 0.2
-var ACCELERATION = 40
+const VECTOR_UP = Vector2(0, -1)
+const MAXSPEED = 300
+const FRICTION = 0.2
+const ACCELERATION = 40
+const AIRCONTROLRATIO = 0.1
+const GRAVITY = 600
+const JUMPFORCE = 300
 
 var velocity : Vector2
 
@@ -17,6 +20,7 @@ func _physics_process(delta):
 	var right = Input.is_action_pressed(getMyInput("Right"))
 	var left = Input.is_action_pressed(getMyInput("Left"))
 	var up = Input.is_action_pressed(getMyInput("Up"))
+	var jump = Input.is_action_just_pressed(getMyInput("Up"))
 	var down = Input.is_action_pressed(getMyInput("Down"))
 	if(right != left):
 		if(right):
@@ -25,6 +29,9 @@ func _physics_process(delta):
 			velocity.x = clamp(velocity.x - ACCELERATION, -MAXSPEED, MAXSPEED)
 	else:
 		velocity.x = lerp(velocity.x, 0, FRICTION)
+	if(jump && is_on_floor()):
+		velocity.y = -JUMPFORCE
+	velocity.y += GRAVITY * delta
 	velocity = move_and_slide(velocity, VECTOR_UP)
 
 
